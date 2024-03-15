93ad8e406950c9ed15f437bbe1ff5b5f7c7a1aae
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-15 15:44:01 +0530

    added the barchart component

--- src/App.js
+++ src/App.js
@@ -39,6 +39,29 @@ const App = () => {
           />
           <Tooltip />
         </PieChart>
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 80,
+            bottom: 5,
+          }}
+          barSize={20}
+        >
+          <XAxis
+            dataKey="name"
+            scale="point"
+            padding={{ left: 10, right: 10 }}
+          />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="users" fill="#8884d8" background={{ fill: "#eee" }} />
+        </BarChart>
     </div>
     </div>
   );
