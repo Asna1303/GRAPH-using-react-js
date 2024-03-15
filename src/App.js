f6ba5f2bbafba4c1e0a3c6ce2a4ba4358541ce1c
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-15 15:25:13 +0530

    added the function to add data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const data = [
+    { name: "Facebook", users: 2000000000 },
+    { name: "Instagram", users: 1500000000 },
+    { name: "Twiter", users: 1000000000 },
+    { name: "Telegram", users: 500000000 },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Chart!</h1>
     </div>
   );
 }
